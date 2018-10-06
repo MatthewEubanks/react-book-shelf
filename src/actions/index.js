98d6540e0cdbfde06f80cadafcc38c99a8b95f82
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-10-06 11:07:15 -0700

    fix again

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-const url = 'https://dry-waters-53761.herokuapp.com';
-//const url = 'http://localhost:3001';
+// const url = 'https://dry-waters-53761.herokuapp.com';
+// const url = 'http://localhost:3001';
 
 export function getBooks(limit = 10, start = 0, order = 'asc', list = '') {
+  // const request = axios(url, {});
+
   const request = axios
-    .get(`${url}/api/books?limit=${limit}&skip=${start}&order=${order}`)
+    .get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
     .then(response => {
       if (list) {
         return [...list, ...response.data];
@@ -20,25 +22,23 @@ export function getBooks(limit = 10, start = 0, order = 'asc', list = '') {
 }
 
 export function getBookWithReviewer(id) {
-  const request = axios.get(`${url}/api/getBook?id=${id}`);
+  const request = axios.get(`/api/getBook?id=${id}`);
 
   return dispatch => {
     request.then(({ data }) => {
       let book = data;
 
-      axios
-        .get(`${url}/api/getReviewer?id=${book.ownerId}`)
-        .then(({ data }) => {
-          let response = {
-            book,
-            reviewer: data,
-          };
+      axios.get(`/api/getReviewer?id=${book.ownerId}`).then(({ data }) => {
+        let response = {
+          book,
+          reviewer: data,
+        };
 
-          dispatch({
-            type: 'GET_BOOK_W_REVIEWER',
-            payload: response,
-          });
+        dispatch({
+          type: 'GET_BOOK_W_REVIEWER',
+          payload: response,
         });
+      });
     });
   };
 }
@@ -54,9 +54,7 @@ export function clearBookWithReviewer() {
 }
 
 export function addBook(book) {
-  const request = axios
-    .post(`${url}/api/book`, book)
-    .then(response => response.data);
+  const request = axios.post(`/api/book`, book).then(response => response.data);
 
   return {
     type: 'ADD_BOOK',
@@ -72,7 +70,7 @@ export function clearNewBook() {
 
 export function getUserPosts(userId) {
   const request = axios
-    .get(`${url}/api/user_posts?user=${userId}`)
+    .get(`/api/user_posts?user=${userId}`)
     .then(response => response.data);
 
   return {
@@ -83,7 +81,7 @@ export function getUserPosts(userId) {
 
 export function getBook(id) {
   const request = axios
-    .get(`${url}/api/getBook?id=${id}`)
+    .get(`/api/getBook?id=${id}`)
     .then(response => response.data);
 
   return {
@@ -94,7 +92,7 @@ export function getBook(id) {
 
 export function updateBook(data) {
   const request = axios
-    .post(`${url}/api/book_update`, data)
+    .post(`/api/book_update`, data)
     .then(response => response.data);
 
   return {
@@ -105,7 +103,7 @@ export function updateBook(data) {
 
 export function deleteBook(id) {
   const request = axios
-    .delete(`${url}/api/delete_book?id=${id}`)
+    .delete(`/api/delete_book?id=${id}`)
     .then(response => response.data);
 
   return {
@@ -129,7 +127,7 @@ export function clearBook() {
 
 export function loginUser({ email, password }) {
   const request = axios
-    .post(`${url}/api/login`, { email, password })
+    .post(`/api/login`, { email, password })
     .then(response => response.data);
 
   return {
@@ -139,7 +137,7 @@ export function loginUser({ email, password }) {
 }
 
 export function auth() {
-  const request = axios.get(`${url}/api/auth`).then(response => response.data);
+  const request = axios.get(`/api/auth`).then(response => response.data);
 
   return {
     type: 'USER_AUTH',
@@ -148,7 +146,7 @@ export function auth() {
 }
 
 export function getUsers() {
-  const request = axios.get(`${url}/api/users`).then(response => response.data);
+  const request = axios.get(`/api/users`).then(response => response.data);
 
   return {
     type: 'GET_USER',
@@ -157,7 +155,7 @@ export function getUsers() {
 }
 
 export function userRegister(user, userList) {
-  const request = axios.post(`${url}/api/register`, user);
+  const request = axios.post(`/api/register`, user);
 
   return dispatch => {
     request.then(({ data }) => {
